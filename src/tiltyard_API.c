4cb3cf1c6f2ccd8d0b2569aedd0c9782387c0a27
The-Assembly-Knight <thecodingknight255@gmail.com>
2025-08-20 17:03:23 -0400

    docs: add comments to all functions of the API

--- src/tiltyard_API.c
+++ src/tiltyard_API.c
@@ -5,6 +5,24 @@
 
 #include "../include/tiltyard_API.h"
 
+/* Create a new yard with size 'capacity'.
+ *
+ * Create space in the heap for the yard
+ * through malloc
+ *
+ * Returns:
+ * - A null pointer if the capacity is 0 or
+ *   there is not enough memory in the heap for that capacity or the yard pointer.
+ * - A pointer to a yard allocated in the heap if there is enough
+ *   memory in the heap for the capacity given.
+ *
+ * Notes:
+ * - Allocates memory in the heap through malloc 2 times, one for the yard
+ *   and the other one for the base of the yard (capacity).
+ * - The memory allocated in the heap for the yard and its base
+ *   must be freed through tiltyard_destroy, tiltyard_destroy_and_null, or
+ *   tiltyard_wipe_destroy_and_null functions.
+ */
 Yard *tiltyard_create(size_t capacity)
 {
 	if (capacity == 0)
@@ -24,11 +42,39 @@ Yard *tiltyard_create(size_t capacity)
 	return yard;
 }
 
+/* Allocate 'size' bytes from the yard with the default alignmnet
+ *
+ * The default alignment is sizeof(void *).
+ *
+ * Returns:
+ * - A pointer into the yard if there is enough capacity.
+ * - NULL if there is not enough space or yard == NULL.
+ *
+ * Notes:
+ * - Does NOT call malloc/free; the returned pointer comes
+ *   from the already allocated memory for the yard.
+ * - The memory is uninitialized (use tiltyard_calloc if you need
+ *   zeroed memory)
+ */
 void *tiltyard_alloc(Yard *yard, size_t size)
 {
 	return tiltyard_alloc_aligned(yard, size, sizeof(void *));
 }
 
+/* Allocate size bytes from the yard with the default alignment,
+ * and zero-initialize the memory.
+ *
+ * Same behavior as 'tiltyard_alloc' except:
+ * - The returned memory is set to all zero bytes.
+ *
+ * Returns:
+ * - A pointer into the yard if there is enough capacity.
+ * - NULL if there is not enough space or yard == NULL.
+ *
+ * Notes:
+ * - Does NOT call malloc/free; the returned pointer comes
+ *   from the already allocated memory for the yard.
+ */
 void *tiltyard_calloc(Yard *yard, size_t size)
 {
 	void *ptr = tiltyard_alloc(yard, size);
@@ -39,6 +85,22 @@ void *tiltyard_calloc(Yard *yard, size_t size)
 	return ptr;
 }
 
+/* Allocate size bytes from the yard with a custom alignment.
+ *
+ * Same behavior as 'tiltyard_alloc' except:
+ * - The memory is aligned based on 'alignment' instead of a
+ *   default one.
+ *
+ * Returns:
+ * - A pointer into the yard if there is enough capacity.
+ * - NULL if there is not enough space or yard == NULL.
+ *
+ * Notes:
+ * - Does NOT call malloc/free; the returned pointer comes
+ *   from the already allocated memory for the yard.
+ * - The memory is uninitialized (use tiltyard_calloc_aligned if you need
+ *   zeroed memory)
+ */
 void *tiltyard_alloc_aligned(Yard *yard, size_t size, size_t alignment)
 {
 	if (!yard || size == 0 || alignment == 0 || (alignment & (alignment - 1)) != 0)
@@ -54,6 +116,21 @@ void *tiltyard_alloc_aligned(Yard *yard, size_t size, size_t alignment)
 	return ptr;
 }
 
+/* Allocate size bytes from the yard with a custom alignment,
+ * and zero-initialize the memory.
+ *
+ * Same behavior as 'tiltyard_calloc' except:
+ * - The memory is aligned based on 'alignment' instead of a
+ *   default one.
+ *
+ * Returns:
+ * - A pointer into the yard if there is enough capacity.
+ * - NULL if there is not enough space or yard == NULL.
+ *
+ * Notes:
+ * - Does NOT call malloc/free; the returned pointer comes
+ *   from the already allocated memory for the yard.
+ */
 void *tiltyard_calloc_aligned(Yard *yard, size_t size, size_t alignment)
 {
 	void *ptr = tiltyard_alloc_aligned(yard, size, alignment);
@@ -64,7 +141,24 @@ void *tiltyard_calloc_aligned(Yard *yard, size_t size, size_t alignment)
 	return ptr;
 }
 
-
+/* Frees yard and its based (which are allocated in the heap)
+ *
+ * the yard's base and the yard itself will be freed using free
+ * if 'yard' is not NULL.
+ *
+ * Returns:
+ * - Nothing
+ *
+ * Notes:
+ * - The pointer to yard will still point to where it pointed, even
+ *   though the memory was already freed, in case you want to null the pointer
+ *   please consider using 'tiltyard_null', 'tiltyard_destroy_and_null',
+ *   or 'tiltyard_wipe_destroy_and_null'.
+ * - Until the OS process that the memory was freed, it will still contain
+ *   what it already had before freeing, in case you waant to zero what you
+ *   wrote on that memory please considere using 'tiltyard_wipe'
+ *   or 'tiltyard_wipe_destroy_and_null'
+ */
 void tiltyard_destroy(Yard *yard) 
 {
 	if (!yard) return;
@@ -73,6 +167,18 @@ void tiltyard_destroy(Yard *yard)
 	free(yard);
 }
 
+/* Zeroes all the memory in the yard.
+ *
+ * Uses memset to zero the entire yard if the 'yard' is not NULL
+ *
+ * Returns:
+ * - Nothing
+ *
+ * Notes:
+ * - Take into account that this function will not free the memory allocated
+ *   for the yard nor null the yard. In case you want to do any of those actions,
+ *   please consider using 'tiltyard_wipe_destroy_and_null'.
+ */
 void tiltyard_wipe(Yard *yard)
 {
 	if (!yard) return;
@@ -80,6 +186,23 @@ void tiltyard_wipe(Yard *yard)
 	memset(yard->base, 0, yard->capacity);
 }
 
+/* Nulls the pointer to the yard given by the user.
+ *
+ * Nulls 'yard' if 'yard' is not already NULL.
+ *
+ * Returns:
+ * - Nothing
+ *
+ * Notes:
+ * - Take into account that if the pointer is null
+ *   there will not be any way to recover the yard, which
+ *   means there will not be any way to free it.
+ * - If you want to free the yard, use this function after you used
+ *   'tiltyard_destroy'.
+ * - The yard will keep containing what was allocated before it was nulled,
+ *   so please make sure you use 'tiltyard_wipe' before this function
+ *   or instead use 'tiltyard_wipe_destroy_and_null'.
+ */
 void tiltyard_null(Yard **yard)
 {	
 	if (!yard) return;
@@ -87,6 +210,20 @@ void tiltyard_null(Yard **yard)
 	*yard = NULL;
 }
 
+/* Destroys and nulls the yard in that order
+ *
+ * If the given yard is not NULL,
+ * combines the behaviors of 'tiltyard_destroy' and
+ * 'tiltyard_null' in that order.
+ *
+ * Notes:
+ * - Take into account that the pointer to the yard
+ *   will be null making it impossible to access the yard again.
+ * - Take into account that this function will not prevent the memory
+ *   where the yard was allocated to keep containing what it had before
+ *   it was destroyed and null. In case you want to make sure the memory was
+ *   zeroed use 'tiltyard_wipe_destroy_and_null'.
+ */
 void tiltyard_destroy_and_null(Yard **yard)
 {
 	if (!yard || !*yard) return;
@@ -95,7 +232,20 @@ void tiltyard_destroy_and_null(Yard **yard)
 	tiltyard_null(yard);
 }
 
-void tiltyard_destroy_wipe_and_null(Yard **yard)
+/* Wipes, destroys, and nulls the yard in that order.
+ *
+ * If 'yard' is not NULL, combines the behaviors of
+ * 'tiltyard_wipe' and 'tiltyard_destroy_and_null' in
+ * that order.
+ *
+ * Returns:
+ * - Nothing
+ *
+ * Notes:
+ * - Take into account that the pointer to the yard
+ *   will be null making it impossible to access the yard again.
+ */
+void tiltyard_wipe_destroy_and_null(Yard **yard)
 {
 	if (!yard || !*yard) return;
 
@@ -103,11 +253,35 @@ void tiltyard_destroy_wipe_and_null(Yard **yard)
 	tiltyard_destroy_and_null(yard);
 }
 
+/* Resets the current offset to 0
+ *
+ * Allows the user to reuse the arena from the beginnig
+ * by reseting the offset to 0 if the 'yard' is not NULL.
+ *
+ * Return:
+ * - Nothing.
+ *
+ * Notes:
+ * - Even though the user resets the yard, the yard will still
+ *   hold the data that was allocated into it.
+ * - In case you want to reset the yard and reset all the data within it,
+ *   you should use 'tiltyard_wipe' followed by this function.
+ */
 void tiltyard_reset(Yard *yard)
 {
 	if (yard) yard->offset = 0;
 }
 
+/* Gets current offset as a marker.
+ *
+ * Returns:
+ * - SIZE_MAX if 'yard' is NULL.
+ * - Current offset if 'yard' is not NULL.
+ *
+ * Notes:
+ * - The user should check if the marker is SIZE_MAX
+ *   as a way to check if this function worked as it is supposed to.
+ */
 size_t tiltyard_get_marker(Yard *yard)
 {
 	if (!yard) return SIZE_MAX;
@@ -115,6 +289,18 @@ size_t tiltyard_get_marker(Yard *yard)
 	return yard->offset;
 }
 
+/* Changes the current offset to 'marker'
+ *
+ * Changes the current offset to 'marker' as long
+ * as 'yard' is not NULL and marker is <= the yard's capacity.
+ *
+ * Returns:
+ * - Nothing
+ *
+ * Notes:
+ * - Even if the offset offset is reset to marker, 
+ *   the yard will conserve the data it had before.
+ */
 void tiltyard_reset_to(Yard *yard, size_t marker)
 {
 	if (!yard || marker > yard->capacity)
@@ -123,6 +309,18 @@ void tiltyard_reset_to(Yard *yard, size_t marker)
 	yard->offset = marker;
 }
 
+/* Zeroes all bytes from the beginning of the yard
+ * to the marker.
+ *
+ * Zeroes all bytes from the beginning of the yard
+ * to the marker using memset if yard is not NULL, marker is not 0,
+ * and marker is not > yard's capacity.
+ *
+ * Note:
+ * - The data zeroed will not be recoverable through tiltyard by
+ *   any means, which means the user should think twice before using
+ *   this function.
+ */
 void tiltyard_clean_until(Yard *yard, size_t marker)
 {
 	if (!yard || marker == 0 || marker > yard->capacity)
@@ -131,6 +329,17 @@ void tiltyard_clean_until(Yard *yard, size_t marker)
 	memset(yard->base, 0, marker);
 }
 
+/* Zeroes all bytes from the maker to the end of the yard
+ *
+ * Zeroes all bytes from the maker
+ * to the end of the yard  using memset
+ * if yard is not NULL, and the marker is <= yard's capacity.
+ *
+ * Note:
+ * - The data zeroed will not be recoverable through tiltyard by
+ *   any means, which means the user should think twice before using
+ *   this function.
+ */
 void tiltyard_clean_from(Yard *yard, size_t marker)
 {
 	if (!yard || marker >= yard->capacity)
@@ -139,6 +348,18 @@ void tiltyard_clean_from(Yard *yard, size_t marker)
 	memset((char *)yard->base + marker, 0, yard->capacity - marker);
 }
 
+/* Zeroes all bytes from 'maker_beg' to 'marker_end'
+ *
+ * Zeroes all bytes from 'maker_beg'
+ * to 'marker_end'  using memset
+ * if yard is not NULL, 'maker_beg' < 'maker_end',
+ * 'marker_beg' < yard's capacity, and 'marker_end' <= yard's capacity.
+ *
+ * Note:
+ * - The data zeroed will not be recoverable through tiltyard by
+ *   any means, which means the user should think twice before using
+ *   this function.
+ */
 void tiltyard_clean_from_until(Yard *yard, size_t marker_beg, size_t marker_end)
 {
 	if (!yard || marker_beg >= marker_end || marker_beg >= yard->capacity || marker_end > yard->capacity)
@@ -147,18 +368,57 @@ void tiltyard_clean_from_until(Yard *yard, size_t marker_beg, size_t marker_end)
 	memset((char *)yard->base + marker_beg, 0, marker_end - marker_beg);
 }
 
+/* Returns the amount of bytes used already
+ *
+ * Returns yard's offset if yard is not NULL.
+ *
+ * Returns:
+ * - SIZE_MAX if 'yard' is NULL.
+ * - yard's offset if 'yard' is not NULL.
+ *
+ * Notes:
+ *  - This function is meant to be used as a way to debug
+ *  the yards already created and it will not affect nor change
+ *  any aspect of the yard.
+ */
 size_t tiltyard_get_used_capacity(Yard *yard)
 {
 	if (!yard) return SIZE_MAX;
 	return yard->offset;
 }
 
+/* Returns the capacity of the yard
+ *
+ * Returns yard's capacity if yard is not NULL.
+ *
+ * Returns:
+ * - SIZE_MAX if 'yard' is NULL.
+ * - yard's capacity if 'yard' is not NULL.
+ *
+ * Notes:
+ *  - This function is meant to be used as a way to debug
+ *  the yards already created and it will not affect nor change
+ *  any aspect of the yard.
+ */
 size_t tiltyard_get_capacity(Yard *yard)
 {
 	if (!yard) return SIZE_MAX;
 	return yard->capacity;
 }
 
+/* Return the amount of bytes available until the end of the yard
+ *
+ * Returns yard's available capacity if yard is not NULL.
+ *
+ * Returns:
+ * - SIZE_MAX if 'yard' is NULL.
+ * - yard's capacity - yard's offset if 'yard' is not NULL.
+ *
+ * Notes:
+ *  - This function is meant to be used as a way to debug
+ *  the yards already created and it will not affect nor change
+ *  any aspect of the yard.
+ */
 size_t tiltyard_get_available_capacity(Yard *yard)
 {
 	if (!yard) return SIZE_MAX;
