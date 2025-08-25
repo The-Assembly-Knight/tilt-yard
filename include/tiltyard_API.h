d599119e9f0c0a4e387b3f4a8a5a352bee91fc66
The-Assembly-Knight <thecodingknight255@gmail.com>
2025-08-24 21:51:27 -0400

    chore: move Yard struct from its own file to inside API.h

--- include/tiltyard_API.h
+++ include/tiltyard_API.h
@@ -1,6 +1,17 @@
 #pragma once
 
-#include "tiltyard_struct.h"
+#include <sys/types.h>
+#include <stdint.h>
+
+typedef struct {
+	uint8_t *base;
+	size_t capacity;
+	size_t offset;
+
+	size_t last_alloc_offset;
+	size_t high_water;
+	size_t alloc_count;
+} Yard;
 
 typedef struct {
 	size_t capacity;
