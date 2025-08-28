b24027b4aceba1691e1c3d462c66a79f75c38008
The-Assembly-Knight <thecodingknight255@gmail.com>
2025-08-27 22:27:51 -0400

    chore: improved error_handling (just added basic structure of error handling for now)

--- src/tiltyard_Error.c
+++ src/tiltyard_Error.c
@@ -1,12 +1,33 @@
-#include <stdio.h>
-#include <assert.h>
-
 #include "../include/tiltyard_Error.h"
 
-void assert_error(const int condition, const char *error_message)
+static char *error_code_strings[TILTYARD_ERROR_CODE_AMOUNT] = {
+	""
+};
+
+static char *func_strings[TILTYARD_FUNC_AMOUNT] = {
+	""
+};
+
+static char *get_error_code_string(const enum tiltyard_error_code code)
 {
-	if (condition) {
-		fprintf(stderr, "%s", error_message);
-		assert(!condition);
-	}
+	if (code > TILTYARD_ERROR_CODE_AMOUNT)
+		return 0;
+
+	return error_code_strings[code];
+}
+
+static char *get_func_string(const enum tiltyard_func func)
+{
+	if (func > TILTYARD_FUNC_AMOUNT)
+		return 0;
+
+	return func_strings[func];
+}
+
+
+void handle_error(const enum tiltyard_error_code error_code,
+		  const enum tiltyard_func in_func,
+		  const bool fatal
+)
+{
 }
